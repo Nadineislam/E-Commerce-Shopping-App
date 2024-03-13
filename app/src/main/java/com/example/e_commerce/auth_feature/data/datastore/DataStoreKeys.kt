bda38afd3b3bfe5237eaf36a4720fe7611bb5cec
Nadine Islam <nadeen.kamaal@gmail.com>
2024-03-13 15:27:21 +0200

    Refactor DataStore instances to manage separate sets of preferences efficiently

--- app/src/main/java/com/example/e_commerce/auth_feature/data/datastore/DataStoreKeys.kt
+++ app/src/main/java/com/example/e_commerce/auth_feature/data/datastore/DataStoreKeys.kt
@@ -1,10 +1,17 @@
 package com.example.e_commerce.auth_feature.data.datastore
 
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import com.example.e_commerce.auth_feature.data.datastore.DataStoreKeys.E_COMMERCE_PREFERENCES
 
 object DataStoreKeys {
-    const val USER_PREFERENCES = "user_preferences"
+    const val E_COMMERCE_PREFERENCES = "e_commerce_preferences"
     val IS_USER_LOGGED_IN = booleanPreferencesKey("is_user_logged_in")
     val USER_ID = stringPreferencesKey("user_id")
 }
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = E_COMMERCE_PREFERENCES)
