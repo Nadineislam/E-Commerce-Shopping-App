f4268f985526afb95f57d1a231466a6de72554d9
Nadine Islam <nadeen.kamaal@gmail.com>
2024-03-31 13:24:46 +0200

    implement LoginWithGoogleUseCase class

--- app/src/main/java/com/example/e_commerce/auth_feature/domain/use_case/LoginWithGoogleUseCase.kt
+++ app/src/main/java/com/example/e_commerce/auth_feature/domain/use_case/LoginWithGoogleUseCase.kt
@@ -3,8 +3,9 @@ package com.example.e_commerce.auth_feature.domain.use_case
 import com.example.e_commerce.auth_feature.domain.repository.FirebaseAuthRepository
 import com.example.e_commerce.core.utils.Resource
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-class LoginWithGoogleUseCase(private val firebaseAuthRepository: FirebaseAuthRepository) {
+class LoginWithGoogleUseCase @Inject constructor(private val firebaseAuthRepository: FirebaseAuthRepository) {
 
     suspend operator fun invoke(idToken: String): Flow<Resource<String>> {
         return firebaseAuthRepository.loginWithGoogle(idToken)
